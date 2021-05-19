2afb4da19f497e6f970995f77968f136fbadb177
tyler <tyler.dojcak@gmail.com>
2021-05-19 15:16:53 -0400

    added book model

--- book_reviews/models.py
+++ book_reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    description = models.TextField()
+    publish_date = models.DateField
+    purchase_link = models.CharField()
+    reviews = []
