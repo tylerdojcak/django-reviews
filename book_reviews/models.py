634fe48a540cfe7fd2df1e124d6d66c5bc51ecc3
tyler <tyler.dojcak@gmail.com>
2021-05-19 15:42:00 -0400

    migrations and book model

--- book_reviews/models.py
+++ book_reviews/models.py
@@ -8,5 +8,5 @@ class Book(models.Model):
     author = models.CharField(max_length=250)
     description = models.TextField()
     publish_date = models.DateField
-    purchase_link = models.CharField()
+    purchase_link = models.CharField(max_length=450, default='http://www.amazon.com')
     reviews = []
