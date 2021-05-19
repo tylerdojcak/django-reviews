7e04046f9c371279411a487e00ca7afc3c151d47
tyler <tyler.dojcak@gmail.com>
2021-05-19 18:17:39 -0400

    context

--- book_reviews/views.py
+++ book_reviews/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'template.html')
+    count = Book.objects.all().count()
+    context = {
+        'count': count,
+    }
+    return render(request, 'template.html', context)
 
 def books(request):
-    return render(request, 'books.html')
+    count = Book.objects.all().count()
+    context = {
+        'count': count,
+    }
+    return render(request, 'books.html', context)
