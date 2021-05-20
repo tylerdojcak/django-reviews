b0eb5b3ceecca97bc966d34fb19290af06ad262b
tyler <tyler.dojcak@gmail.com>
2021-05-20 16:55:35 -0400

    bootstrap nav

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,6 +9,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('index')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
--- reviews/settings.py
+++ reviews/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'book_reviews',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
