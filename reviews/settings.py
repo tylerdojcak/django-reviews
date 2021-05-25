c01d420a4b1d1c5a77e88406d0298220dc05f4f9
tyler <tyler.dojcak@gmail.com>
2021-05-25 12:40:24 -0400

    registration

--- reviews/settings.py
+++ reviews/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'crispy_forms',
     'book_reviews',
     'accounts',
 ]
@@ -130,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
