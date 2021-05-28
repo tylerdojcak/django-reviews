d73cd1b986960b787d99b1a21dd03d87e17cb56d
tyler <tyler.dojcak@gmail.com>
2021-05-28 11:45:48 -0400

    extra context to login page

--- reviews/urls.py
+++ reviews/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import include, path
 from book_reviews import views as book_review_views
 from accounts import views as account_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', book_review_views.index, name='index'),
     path('accounts/', account_views.register, name='register'),
     path('books/', include('book_reviews.urls'), name='books'),
     path('books/add', book_review_views.add_book, name='add_book'),
-    path('', include('django.contrib.auth.urls'), {'extra_context':{'title': 'Login'}}, name='login'),
+    path('login/', auth_views.LoginView.as_view(extra_context={'title': 'test extra_content'}), name='login'), # override
+    path('', include('django.contrib.auth.urls'), name='login'),
     path('admin/', admin.site.urls),
 ]
